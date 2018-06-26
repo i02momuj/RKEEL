93289d519ca3f121dc026ba3d7a12f0bcfbbe852
Jose María Moyano <i02momuj@uco.es>
2018-06-26 09:16:08 +0200

    Version 1.1.25 of RKEEL

--- RKEEL/R/KeelAlgorithm.R
+++ RKEEL/R/KeelAlgorithm.R
@@ -12,6 +12,14 @@ KeelAlgorithm <- R6::R6Class("KeelAlgorithm",
 
     #Initialize
     initialize = function(){
+      
+      rJava::.jinit()
+      jv <- rJava::.jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+      if(substr(jv, 1L, 1L) == "1") {
+        jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+        if(jvn < 1.8) stop("Java 8 is needed for this package but not available")
+      }
+      
       rJava::.jinit()
       javaPath <- paste0(rJava::.jcall('java/lang/System', 'S', 'getProperty', 'java.home'), "\\bin\\")
 
